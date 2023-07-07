fe16717467fb25c5978dc9ebc12945a5717ca93f
Pigor <higorribeirosantos@gmail.com>
2023-07-07 02:05:51 -0300

    Fix: Resolve error bild

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,9 @@ import '@/styles/card.css';
 import '@/styles/nav.css';
 import '@/styles/index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { AppProps } from 'next/app';
 import { SessionProvider } from 'next-auth/react';
 
-export default function App({ Component, pageProps, session }: AppProps) {
+export default function App({ Component, pageProps, session }: any) {
   return (
     <SessionProvider session={session} refetchInterval={5 * 60}>
       <Component {...pageProps} />
